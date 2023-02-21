581cf27fa3c62a085e9d5fdcfef0c8c33abf5fd3
Gianluca Tiengo <gl.tiengo@gmail.com>
2023-02-21 09:09:01 +0100

    solving all console error

--- src/main.js
+++ src/main.js
@@ -4,4 +4,4 @@ import App from './App.vue'
 import router from "./router.js";
 import i18n from './i18n'
 
-createApp(App).use(i18n).use(router).use(i18n).mount('#app')
+createApp(App).use(router).use(i18n).mount('#app')
